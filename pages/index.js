8d6537575a1360696f8e776179ca236b2900ca59
Joaquín Miglierini <79928122+jmiglierini@users.noreply.github.com>
2022-06-13 20:03:50 +0200

    Create index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Hola!" />
+        <Header title="HOLAAAAAAA!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
